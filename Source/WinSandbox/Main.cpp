0117133214dafb253a108a68643a895a36263d19
Whelan Callahan <wcallahan@wi.rr.com>
2019-09-19 23:58:30 -0400

    added glfw window

--- Source/WinSandbox/Main.cpp
+++ Source/WinSandbox/Main.cpp
@@ -1,7 +1,52 @@
+#include <glad/glad.h>
+#include <glfw3.h>
 #include "Freight.hpp"
+#include <cmath>
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
 
 int main(int argc, char **argv)
 {
-    FR8_DEBUG_LOG("test");
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        FR8_DEBUG_LOG("Failed to create GLFW window");
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        FR8_DEBUG_LOG("Failed to initialize GLAD");
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    float color = 0.0f;
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClearColor((sin(color) + 1) / 2, (sin(color*3) + 1) / 2, (sin(color*6) + 1) / 2, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        color += 0.01f;
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+
     return 0;
 }
